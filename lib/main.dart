555dbccf14da8cd30c26109c9426755c31e0f7ad
Jesus <Jesus.fy.1997@gmail.com>
2022-07-19 21:57:06 -0300

    Nuevo commit

--- lib/main.dart
+++ lib/main.dart
@@ -42,21 +42,31 @@ class _HomePageState extends State<HomePage> {
           },
         ),
       ),
+      body: const Center(
+        child: Text('IG: Jesus_fy_'),
+      ),
     );
   }
 }
 
-class DrawerWidget extends StatelessWidget {
+class DrawerWidget extends StatefulWidget {
   const DrawerWidget({Key? key}) : super(key: key);
 
   @override
+  State<DrawerWidget> createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  var isEnabled = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
+    var size = MediaQuery.of(context).size;
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 1000),
       margin: const EdgeInsets.only(top: 20, left: 20),
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-      width: MediaQuery.of(context).size.width * 0.20,
-      height: MediaQuery.of(context).size.height * 0.9,
+      width: isEnabled ? size.width * 0.8 : size.width * 0.20,
+      height: size.height * 0.9,
       decoration: const BoxDecoration(
         color: Colors.black,
         borderRadius: BorderRadius.all(
@@ -64,37 +74,54 @@ class DrawerWidget extends StatelessWidget {
         ),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const FlutterLogo(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: const FlutterLogo(),
+          ),
           const Padding(
             padding: EdgeInsets.symmetric(vertical: 15),
             child: Divider(color: Colors.grey, thickness: 1),
           ),
-          const Icon(Icons.home_outlined, color: Colors.white),
-          const SizedBox(height: 15),
-          const Icon(Icons.calendar_today, color: Colors.white),
-          const SizedBox(height: 15),
-          const Icon(Icons.pin_drop_rounded, color: Colors.white),
-          const SizedBox(height: 15),
-          const Icon(Icons.message, color: Colors.white),
-          const SizedBox(height: 15),
-          const Icon(Icons.cloud, color: Colors.white),
-          const SizedBox(height: 15),
-          const Icon(Icons.airplane_ticket, color: Colors.white),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: Column(
+              children: const [
+                Icon(Icons.home_outlined, color: Colors.white),
+                SizedBox(height: 15),
+                Icon(Icons.calendar_today, color: Colors.white),
+                SizedBox(height: 15),
+                Icon(Icons.pin_drop_rounded, color: Colors.white),
+                SizedBox(height: 15),
+                Icon(Icons.message, color: Colors.white),
+                SizedBox(height: 15),
+                Icon(Icons.cloud, color: Colors.white),
+                SizedBox(height: 15),
+                Icon(Icons.airplane_ticket, color: Colors.white),
+              ],
+            ),
+          ),
           const Padding(
             padding: EdgeInsets.symmetric(vertical: 15),
             child: Divider(color: Colors.grey, thickness: 1),
           ),
           const Spacer(),
-          const Icon(Icons.notifications, color: Colors.white),
-          const SizedBox(height: 15),
-          const Icon(Icons.settings, color: Colors.white),
-          const SizedBox(height: 15),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.14,
-            height: MediaQuery.of(context).size.height * 0.12,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: Column(
+              children: const [
+                Icon(Icons.notifications, color: Colors.white),
+                SizedBox(height: 15),
+                Icon(Icons.settings, color: Colors.white),
+                SizedBox(height: 15),
+              ],
+            ),
+          ),
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            width: isEnabled ? size.width * 0.8 : size.width * 0.20,
+            height: size.height * 0.12,
             padding: const EdgeInsets.all(10),
             decoration: const BoxDecoration(
               color: Color(0xff2C2C2C),
@@ -102,16 +129,36 @@ class DrawerWidget extends StatelessWidget {
                 Radius.circular(22),
               ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                CircleAvatar(),
-                Icon(Icons.logout, color: Colors.white),
-              ],
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.02),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/pana_miguel.jpg',
+                    ),
+                  ),
+                  Icon(Icons.logout, color: Colors.white),
+                ],
+              ),
             ),
           ),
           const SizedBox(height: 15),
-          const Icon(Icons.keyboard_arrow_right_rounded, color: Colors.white),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isEnabled = !isEnabled;
+                });
+              },
+              child: const Icon(
+                Icons.keyboard_arrow_right_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
     );
